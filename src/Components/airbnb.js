f8228419156b84bcf51820231524e038d395f128
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-02-16 23:33:34 +0530

    react-jsx-002

---
+++ src/Components/airbnb.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+export default function Airbnb() {
+    return (
+        <div>
+            <h1 style={{color: 'white'}}>
+                The Evolution of Airbnb's Frontend
+            </h1>
+            <h6 style={{color: 'white'}}>@spikebrehm</h6>
+            <img src="https://www.doz.com/wp-content/uploads/2015/03/airbnb-logo.png" alt="airbnb-logo" />
+        </div>
+    );
+}
